e992a6f838f7c666bc45f62142c2abcd4d694e6c
MRaihanZ <101298615+MRaihanZ@users.noreply.github.com>
2025-07-28 06:44:28 +0700

    feat: remove unused import package

--- internal/model/user_model.go
+++ internal/model/user_model.go
@@ -7,8 +7,6 @@ import (
 
 	"github.com/MRaihanZ/subcommerce-backend/internal/db"
 	"github.com/MRaihanZ/subcommerce-backend/internal/entity"
-	"github.com/google/uuid"
-	"golang.org/x/crypto/bcrypt"
 )
 
 func GetAllUsers() ([]entity.User, error) {
@@ -38,21 +36,4 @@ func GetUserById(id string) (*entity.User, error) {
 	}
 
 	return &user, nil
-}
-
-func CreateUser(name string, email string, dob string, password string) (*uuid.UUID, error) {
-	var id uuid.UUID
-	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
-	if err != nil {
-		return nil, err
-	}
-
-	id = uuid.New()
-	err = db.DB.QueryRowx("INSERT INTO users (id, name, email, dob, password) VALUES ($1, $2, $3, $4, $5) RETURNING id",
-		id, name, email, dob, hashedPassword,
-	).Scan(&id)
-	if err != nil {
-		return nil, err
-	}
-	return &id, nil
 }
