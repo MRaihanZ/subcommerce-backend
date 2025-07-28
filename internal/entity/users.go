acebe7151d5025f04e9aa66cca7dd26a1f18c632
MRaihanZ <101298615+MRaihanZ@users.noreply.github.com>
2025-07-28 10:49:54 +0700

    feat: change field name from Created_at to CreatedAt

--- internal/entity/users.go
+++ internal/entity/users.go
@@ -3,11 +3,11 @@ package entity
 import "time"
 
 type User struct {
-	ID         string    `db:"id" json:"id"`
-	Name       string    `db:"name" json:"name"`
-	Img        string    `db:"img" json:"img"`
-	Email      string    `db:"email" json:"email"`
-	Dob        time.Time `db:"dob" json:"dob"`
-	Password   string    `db:"password" json:"password"`
-	Created_at time.Time `db:"created_at" json:"created_at"`
+	ID        string    `db:"id" json:"id"`
+	Name      string    `db:"name" json:"name"`
+	Img       string    `db:"img" json:"img"`
+	Email     string    `db:"email" json:"email"`
+	Dob       time.Time `db:"dob" json:"dob"`
+	Password  string    `db:"password" json:"password"`
+	CreatedAt time.Time `db:"created_at" json:"created_at"`
 }
