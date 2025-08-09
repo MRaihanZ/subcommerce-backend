7891af27e4824e28fb5d70a6e8d9bc665aa1f749
MRaihanZ <101298615+MRaihanZ@users.noreply.github.com>
2025-08-10 02:17:46 +0700

    feat: change datatype from time.Time to string in users entity

--- internal/entity/users.go
+++ internal/entity/users.go
@@ -1,13 +1,11 @@
 package entity
 
-import "time"
-
 type User struct {
-	ID        string    `db:"id" json:"id"`
-	Name      string    `db:"name" json:"name"`
-	Img       string    `db:"img" json:"img"`
-	Email     string    `db:"email" json:"email"`
-	Dob       time.Time `db:"dob" json:"dob"`
-	Password  string    `db:"password" json:"password"`
-	CreatedAt time.Time `db:"created_at" json:"created_at"`
+	ID        string `db:"id" json:"id"`
+	Name      string `db:"name" json:"name"`
+	Img       string `db:"img" json:"img"`
+	Email     string `db:"email" json:"email"`
+	Dob       string `db:"dob" json:"dob"`
+	Password  string `db:"password" json:"password"`
+	CreatedAt string `db:"created_at" json:"created_at"`
 }
