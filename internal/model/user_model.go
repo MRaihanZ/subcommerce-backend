a46190ffa9f9ab283c4e692904587466bcc1ebf7
MRaihanZ <101298615+MRaihanZ@users.noreply.github.com>
2025-08-10 02:18:57 +0700

    feat: remove unused function in user model

--- internal/model/user_model.go
+++ internal/model/user_model.go
@@ -8,20 +8,6 @@ import (
 	"github.com/MRaihanZ/subcommerce-backend/internal/entity"
 )
 
-func GetAllUsers() ([]entity.User, error) {
-	var users []entity.User
-	err := db.DB.Select(&users, "SELECT * FROM users")
-	if err != nil {
-		return nil, err
-	}
-
-	if len(users) == 0 {
-		return nil, nil
-	}
-
-	return users, nil
-}
-
 func GetUserById(id string) (*entity.User, error) {
 	var user entity.User
 	err := db.DB.Get(&user, "SELECT * FROM users WHERE id = $1", id)
