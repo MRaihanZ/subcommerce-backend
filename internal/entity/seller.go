e29a8ea0ddf432b6d2ee139ad659dee0b7c5fe4b
MRaihanZ <101298615+MRaihanZ@users.noreply.github.com>
2025-08-21 22:29:52 +0700

    feat: re-order struct and add Address field in SellerSummarize struct

--- internal/entity/seller.go
+++ internal/entity/seller.go
@@ -2,14 +2,6 @@ package entity
 
 import "time"
 
-type SellerSummarize struct {
-	Id            string  `db:"id" json:"id"`
-	Name          string  `db:"name" json:"name"`
-	Img           string  `db:"img" json:"img"`
-	Sold          int     `db:"sold_products" json:"sold_products"`
-	AverageRating float32 `db:"average_rating" json:"average_rating"`
-}
-
 type Seller struct {
 	Id            string    `db:"id" json:"id"`
 	Name          string    `db:"name" json:"name"`
@@ -20,4 +12,13 @@ type Seller struct {
 	RatingTotal   int       `db:"rating_total" json:"rating_total"`
 	RatingCount   int       `db:"rating_count" json:"rating_count"`
 	CreatedAt     time.Time `db:"created_at" json:"created_at"`
+}
+
+type SellerSummarize struct {
+	Id            string  `db:"id" json:"id"`
+	Name          string  `db:"name" json:"name"`
+	Img           string  `db:"img" json:"img"`
+	Address       string  `db:"address" json:"address"`
+	Sold          int     `db:"sold_products" json:"sold_products"`
+	AverageRating float32 `db:"average_rating" json:"average_rating"`
 }
